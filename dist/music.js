e6bb047e2f31a630aa1fd991dfb375c3135cb1c5
xiaoqu <1754886979@qq.com>
2021-11-04 22:51:53 +0800

    Site updated: 2021-11-04 22:51:52

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'STAY',
+        artist: 'artist',
+        url: '',
+        cover: 'cover.jpg',
+    }]
+});
